083dd86f23b4ef894f95fca8ad1b96ad2e5eef8a
schambers <schambers80@gmail.com>
2020-06-07 21:37:45 -0500

    more git aliases

--- windows-terminal_alias.ps1
+++ windows-terminal_alias.ps1
@@ -21,3 +21,27 @@ New-Alias -Name c -Value Set-GitCommit
 # usage: co master
 function Get-GitCheckout { & git checkout $args }
 New-Alias -Name co -Value Get-GitCheckout
+
+# git add $args
+# usage: ga -u some/path
+function Get-GitAdd { & git add $args }
+New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
+
+# git push
+# usage: gp
+function Get-GitPush { & git push $args }
+New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+
+# git pull
+# usage: gpu
+function Get-GitPull { & git pull $args }
+New-Alias -Name gpu -Value Get-GitPull -Force -Option AllScope
+
+# git fetch
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name f -Value Get-GitFetch -Force -Option AllScope
+
+# git branch args
+# usage: b new-branch
+function Get-GitBranch { & git branch $args }
+New-Alias -Name b -Value Get-GitBranch -Force -Option AllScope
